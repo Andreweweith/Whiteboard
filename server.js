346a1ec9eaaf95f499c68f69e4f9c8c41db1f6f9
Andrew Weith <Andreweweith@gmail.com>
2020-05-05 02:36:53 -0400

    Fixing weird merge conflicts that ruined everything, like my hopes and dreams.

--- server.js
+++ server.js
@@ -3,40 +3,33 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
 const cors = require('cors');
-const mongoURL = 'mongodb+srv://admin:adminpassword@cluster0-loslm.mongodb.net/test?retryWrites=true&w=majority'
+const MONGOURL = 'mongodb+srv://admin:adminpassword@cluster0-loslm.mongodb.net/test?retryWrites=true&w=majority'
 const app = express();
 app.use(cors());
 
-<<<<<<< HEAD
-mongoose.connect(mongoURL).then(() => console.log("MongoDB Connected")).catch(error => console.log(error));
-=======
 const WebSocket = require('ws');
 
 mongoose.connect(MONGOURL).then(() => console.log("MongoDB Connected")).catch(error => console.log(error));
->>>>>>> temp
 
 const userSchema = mongoose.Schema({
+
     email:{
         type: String,
         required: true,
         unique: 1,
         trim: true
     },
+
     password:{
         type: String,
         required: true,
         minLength: 5
     },
-<<<<<<< HEAD
-=======
 
->>>>>>> temp
     name:{
         type: String,
         required: true,
         minLength: 2
-<<<<<<< HEAD
-=======
     }
 });
 
@@ -82,53 +75,13 @@ const messageSchema = mongoose.Schema({
 
     time_sent:{
         type: String
->>>>>>> temp
     }
 });
-
-const serverSchema = mongoose.Schema({ name: String });
-const serversSchema = mongoose.Schema({
-    server_id:{
-        type: String,
-        required: true,
-        unique: 1,
-        minlength: 8
-    },
-    user_list: {
-        type: [String],
-        required: true
-    },
-    all_messages:{
-        type: mongoose.Schema.Types.ObjectID,
-        ref:'Messages'
-    }
-})
-
-const messageSchema = mongoose.Schema({
-    sender: {
-        type: mongoose.Schema.Types.ObjectID,
-        ref:'User',
-        required: true
-    },
-    content:{
-        type: String,
-        minLength: 1
-    },
-    server_id:{
-        type: mongoose.Schema.Types.ObjectID,
-        ref:'server',
-        minLength: 1
-    },
-    timestamp: String
-})
 
 const User = mongoose.model('User', userSchema);
 const Server = mongoose.model('Server', serverSchema);
 const Messages = mongoose.model('Messages', messageSchema);
-<<<<<<< HEAD
-=======
 
->>>>>>> temp
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -139,8 +92,8 @@ app.use(bodyParser.json());
 app.post("/signup/", async (req, res) => {
     try {
         req.body.password = bcrypt.hashSync(req.body.password, 10);
-        let user = new User(req.body);
-        let ans = await user.save();
+        var user = new User(req.body);
+        var ans = await user.save();
         res.send(ans);
     } catch (err) {
         res.status(500).send(err);
@@ -149,7 +102,7 @@ app.post("/signup/", async (req, res) => {
 
 app.post("/signin/", async (req, res) => {
     try {
-        let user = await User.findOne({ email: req.body.email }).exec();
+        var user = await User.findOne({ email: req.body.email }).exec();
         if(!user)
         {
             return res.status(400).send({ message: "Email not in database" });
